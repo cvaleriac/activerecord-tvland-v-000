f5d66993cf3f30f744cdc8f06186980af4db4686
valeria campilongo <cvaleriac@hotmail.it>
2018-09-28 13:31:30 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,12 +1,11 @@
 class Actor < ActiveRecord::Base
-  class Actor < ActiveRecord::Base
-    has_many :characters
+  has_many :characters
     has_many :shows, through: :characters
-  
+
     def full_name
       name = "#{self.first_name} #{self.last_name}"
     end
-  
+
     def list_roles
       roles = []
       array_of_characters = self.characters.map {|character| character.name}
@@ -15,5 +14,4 @@ class Actor < ActiveRecord::Base
       roles << role.join(" - ")
       roles
     end
-  end  
 end
